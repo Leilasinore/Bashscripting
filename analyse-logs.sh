adafcddfb579347718c1e2224681cad4e1bacfd5
Leila Sinore <leila.sinore@savannahinformatics.com>
2025-12-14 14:23:30 +0300

    optimized script

--- analyse-logs.sh
+++ analyse-logs.sh
@@ -4,15 +4,21 @@ LOG_DIR="/home/leila/logs"
 APP_LOG_FILE="application.log"
 SYS_LOG_FILE="system.log"
 ERROR_PATTERNS=("ERROR" "CRITICAL" "FATAL")
-echo "Locating logs that have changed in the last 24 hrs"
-find $LOG_DIR -name "*.log" -mtime -1
+LOG_FILES=$(find $LOG_DIR -name "*.log")
 
-echo "${ERROR_PATTERNS[0]} logs in application.log file"
-grep -c "${ERROR_PATTERNS[0]}" "$LOG_DIR/$APP_LOG_FILE"
-grep "${ERROR_PATTERNS[0]}" "$LOG_DIR/$APP_LOG_FILE"
-grep -c "${ERROR_PATTERNS[1]}" "$LOG_DIR/$APP_LOG_FILE"
+for LOG_FILE in $LOG_FILES; do
+    echo "logs that have changed in the last 24 hrs"
+    echo -e "\n ${ERROR_PATTERNS[0]} logs in $LOG_FILE file"
+    grep -c "${ERROR_PATTERNS[0]}" "$LOG_DIR/$LOG_FILE"
+    grep "${ERROR_PATTERNS[0]}" "$LOG_DIR/$LOG_FILE"
 
-echo "ERROR logs in system.log file"
-grep -c "${ERROR_PATTERNS[0]}" "$LOG_DIR/$SYS_LOG_FILE"
-grep "${ERROR_PATTERNS[1]}" "$LOG_DIR/$SYS_LOG_FILE"
-grep -c "${ERROR_PATTERNS[1]}" "$LOG_DIR/$SYS_LOG_FILE"
+    echo -e "\n ${ERROR_PATTERNS[1]} logs in $LOG_FILE file"
+    grep -c "${ERROR_PATTERNS[1]}" "$LOG_DIR/$LOG_FILE"
+    grep "${ERROR_PATTERNS[1]}" "$LOG_DIR/$LOG_FILE"
+
+    echo -e "\n ${ERROR_PATTERNS[2]} logs in $LOG_FILE file"
+    grep "${ERROR_PATTERNS[2]}" "$LOG_DIR/$LOG_FILE"
+    grep -c "${ERROR_PATTERNS[2]}" "$LOG_DIR/$LOG_FILE"
+done
+
+
