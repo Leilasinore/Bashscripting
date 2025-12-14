3b7b8229f217f78a52fd62d8485574b8243f1bf7
Leila Sinore <leila.sinore@savannahinformatics.com>
2025-12-14 14:43:49 +0300

    further optimized script to cater for a long range of error patterns

--- analyse-logs.sh
+++ analyse-logs.sh
@@ -7,18 +7,13 @@ ERROR_PATTERNS=("ERROR" "CRITICAL" "FATAL")
 LOG_FILES=$(find $LOG_DIR -name "*.log")
 
 for LOG_FILE in $LOG_FILES; do
-    echo "logs that have changed in the last 24 hrs"
-    echo -e "\n ${ERROR_PATTERNS[0]} logs in $LOG_FILE file"
-    grep -c "${ERROR_PATTERNS[0]}" "$LOG_DIR/$LOG_FILE"
-    grep "${ERROR_PATTERNS[0]}" "$LOG_DIR/$LOG_FILE"
 
-    echo -e "\n ${ERROR_PATTERNS[1]} logs in $LOG_FILE file"
-    grep -c "${ERROR_PATTERNS[1]}" "$LOG_DIR/$LOG_FILE"
-    grep "${ERROR_PATTERNS[1]}" "$LOG_DIR/$LOG_FILE"
-
-    echo -e "\n ${ERROR_PATTERNS[2]} logs in $LOG_FILE file"
-    grep "${ERROR_PATTERNS[2]}" "$LOG_DIR/$LOG_FILE"
-    grep -c "${ERROR_PATTERNS[2]}" "$LOG_DIR/$LOG_FILE"
+    for ERROR_PATTERN in "${ERROR_PATTERNS[@]}"; do
+        echo "logs that have changed in the last 24 hrs"
+        echo -e "\n $ERROR_PATTERN logs in $LOG_FILE file"
+        grep -c "$ERROR_PATTERN" "$LOG_FILE"
+        grep "$ERROR_PATTERN" "$LOG_FILE"
+    done
 done
 
 
